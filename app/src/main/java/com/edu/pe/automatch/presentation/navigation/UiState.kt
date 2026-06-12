c469ffa83548620c56ba3d1fc9644bfe734b7a9f
Romin40 <u202211706@upc.edu.pe>
2026-06-11 19:28:33 -0500

    Delete app/src/main/java/com/edu/pe/automatch/presentation directory

--- app/src/main/java/com/edu/pe/automatch/presentation/navigation/UiState.kt
+++
@@ -1,8 +0,0 @@
-package com.edu.pe.automatch.presentation.navigation
-
-data class UiState(
-    val isLoading: Boolean = false,
-    val errorMessage: String? = null,
-    val isSuccess: Boolean = false,
-    val isMechanic: Boolean = false
-)
